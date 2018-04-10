2eeeb65424692b9e0605017413401a97737fff87
James Babcock <jimrandomh@gmail.com>
2018-04-09 21:06:50 -0400

    Add low-disk space alerts
    This adds two Pushover alerts for low disk space, for low (<50MB) and
    critical (<2MB) free disk space (thresholds configurable). They're
    checked every 15min.
    
    I'll test this by creating some big files to almost-fill my rig, and
    observing that the alerts get sent.

--- bin/oref0-cron-every-15min.sh
+++ bin/oref0-cron-every-15min.sh
@@ -18,3 +18,27 @@ if egrep -i "edison" /etc/passwd 2>/dev/null; then
         | jq .batteryVoltage \
         | awk '{if (\$1<=3050)system(\"sudo shutdown -h now\")}'
 fi
+
+# Get remaining free space (in kb). Whichever partition the log files are on
+# is the one we check.
+FREE_SPACE="$(df /var/log/openaps |tail -1 |awk '{print($4)}')"
+
+# Default thresholds 50MB and 2MB for warning and critical alerts, respectively
+DISK_WARN_THRESHOLD="$(get_pref_float .disk_warn_threshold 50000)"
+DISK_CRITICAL_THRESHOLD="$(get_pref_float .disk_critical_threshold 2000)"
+
+if ((FREE_SPACE < DISK_CRITICAL_THRESHOLD)); then
+    oref0-send-notification \
+        --title "Low disk space on $(hostname)" \
+        --config-prefix disk_warn \
+        --priority 1 \
+        --cooldown 60 \
+        --cooldown-token disk_critical
+elif ((FREE_SPACE < DISK_WARN_THRESHOLD)); then
+    oref0-send-notification \
+        --title "Critically low disk space on $(hostname)" \
+        --config-prefix disk_critical \
+        --priority 0 \
+        --cooldown 720 \
+        --coldown-token disk_warn
+fi
